bfc0872c411704f9afb0c824d3bd40d484189de4
wendell <wendell.hack@gmail.com>
2012-11-18 12:03:25 +0800

    Initial Commit

---
+++ cleanWhiteSpace/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  cleanWhiteSpace
+//
+//  Created by Wendell Capili on 11/18/12.
+//  Copyright (c) 2012 Wendell Capili. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
